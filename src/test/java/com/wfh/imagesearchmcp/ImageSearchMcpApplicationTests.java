74b5246151ff2d46ab6d078f69c5f81fa8ac2e08
wangfenghuan <wfenghuan@foxmail.com>
2025-05-29 14:26:19 +0800

    mcp+WangManus开发

---
+++ src/test/java/com/wfh/imagesearchmcp/ImageSearchMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wfh.imagesearchmcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageSearchMcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
